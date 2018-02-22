b5f74cbd32531ac3846b9940465801fb2c5516d8
carlostrapet <trapet_llamas@hotmail.com>
2018-02-22 17:05:44 +0000

    fixed bug, implemented all user stories. finished business logic

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -30,7 +30,59 @@ describe("Thermostat:", function() {
       };
       expect(function() { thermostat.decrease() }).toThrow("Minimum temperature is 10 degrees")
     });
-
-
   });
+
+  describe("Power saver mode:", function() {
+    it('defaults to on', function(){
+      expect(thermostat._powerSave).toEqual(true);
+    });
+    it('allows to change powerSave to false', function() {
+      thermostat.saverMode();
+      expect(thermostat._powerSave).toEqual(false);
+    });
+
+    it('if powerSave is on, temperature can not go above 25 degrees', function () {
+      for (temp = 20; temp < 25; temp++) {
+        thermostat.increase();
+      };      
+      expect(function () { thermostat.increase() }).toThrow("Maximum temperature with powerSave is 25 degrees")
+    });
+
+    it('if powerSave is off, temperature can not go above 32 degrees', function () {
+      thermostat.saverMode();      
+      for (temp = 20; temp < 32; temp++) {
+        thermostat.increase();
+      };
+      expect(function () { thermostat.increase() }).toThrow("Maximum temperature is 32 degrees")
+    });
+  });
+
+    describe("Reset", function() {
+      it('resets the temperature to 20 degrees', function() {
+        thermostat.increase();
+        thermostat.reset();
+        expect(thermostat._temperature).toEqual(20);
+      });
+    });
+
+    describe("energy usage", function() {
+      it('low energy usage', function () {
+        for (temp = 20; temp > 17; temp --) {
+          thermostat.decrease();
+        };
+        expect(thermostat.energyUsage()).toEqual("low-usage");        
+      });
+      
+      it('medium energy usage', function () {
+        expect(thermostat.energyUsage()).toEqual("medium-usage");        
+      });
+      
+      it('high energy usage', function () {
+        thermostat.saverMode();        
+        for (temp = 20; temp < 26; temp ++) {
+          thermostat.increase();
+        };
+        expect(thermostat.energyUsage()).toEqual("high-usage");        
+      });
+    });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,15 +2,46 @@
 
 function Thermostat() {
   this._temperature = 20;
+  this._powerSave = true;
 };
 
 Thermostat.prototype = {
 
   increase: function() {
+    if (this._temperature === 32) {
+      throw "Maximum temperature is 32 degrees";
+    };
+    if (this._temperature === 25 && this._powerSave === true) {
+      throw "Maximum temperature with powerSave is 25 degrees";
+    }
      this._temperature += 1;
   },
 
   decrease: function() {
-      this._temperature -= 1;
+    if (this._temperature === 10) {
+      throw "Minimum temperature is 10 degrees";
+    };
+    this._temperature -= 1;
+  },
+
+  saverMode: function() {
+    if(this._powerSave === false) {
+      this._powerSave = true;
+    } else if (this._powerSave === true) {
+      this._powerSave = false
+    };
+  },
+  reset: function() {
+    this._temperature = 20;
+  },
+
+  energyUsage: function () {
+    if(this._temperature < 18) {
+      return "low-usage";
+    } else if(this._temperature < 25) {
+      return "medium-usage";
+    } else {
+      return "high-usage";
+    };
   }
 };
