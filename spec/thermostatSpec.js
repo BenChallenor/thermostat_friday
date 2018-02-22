9841837b751d83159b7eaa14bc784d7e45e2914e
carlostrapet <trapet_llamas@hotmail.com>
2018-02-22 14:43:02 +0000

    added first test

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe("Thermostat:", function() {
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+  it("has a default temperature of 20 degrees"), function() {
+    expect(thermostat.temperature()).toEqual(20);
+  };
+});
